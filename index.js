ea504e690cd0ad9be5e2d4896599bf0107aace3c
wajee-ul-hassan <wajeex001@gmail.com>
2024-04-18 19:43:23 +0500

    Html for signin and signup page

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve static files from the public directory
+app.use(express.static('public'));
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
--- routes/signin.js
+++ routes/signin.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path'); // Import the path module
 
-// Route handler for /signin
 router.get('/', (req, res) => {
-    const pageTitle = 'Sign In';
-    res.render('signin', { pageTitle });
+    // Use path.join to get the correct path to the signin.html file in the public folder
+    const signInFilePath = path.join(__dirname, '../public', 'signin.html');
+    res.sendFile(signInFilePath);
 });
 
 module.exports = router;
