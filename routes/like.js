9f767bd3c08f7eee710e7435c19837c9fe224939
afaq-ahmed07 <afaqshahbaz9@gmail.com>
2024-05-16 22:10:51 +0500

    Edit Project Functionality Added

--- routes/like.js
+++ routes/like.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Project = require('../models/projects');
+const SaveProject = require('../models/collections');
 const checkUserLikedStatus = require('../utils/checkUserLikedStatus');
 const cookieParser = require('cookie-parser');
 const authenticateToken = require('../middlewares/auth');
@@ -14,17 +15,43 @@ router.post('/:projectId', async (req, res) => {
     const username = req.user.username; // Assuming user name is available in the request
     try {
         const alreadyLiked = await checkUserLikedStatus(projectId, username);
-
+        const project = await Project.findById(projectId);
+        const saveproject = await SaveProject.findById(projectId);
         let newLikes = currentLikes;
+        if (project) {
 
-        if (!alreadyLiked) {
-            // Increment likes if user has not liked before
-            newLikes++;
-            await Project.findByIdAndUpdate(projectId, { $inc: { likes: 1 }, $push: { likedBy: username } });
-        } else {
-            // Decrement likes if user has already liked before
-            newLikes--;
-            await Project.findByIdAndUpdate(projectId, { $inc: { likes: -1 }, $pull: { likedBy: username } });
+            if (!alreadyLiked) {
+                // Increment likes if user has not liked before
+                newLikes++;
+                await Project.findByIdAndUpdate(projectId, { $inc: { likes: 1 }, $push: { likedBy: username } });
+            } else {
+                // Decrement likes if user has already liked before
+                newLikes--;
+                await Project.findByIdAndUpdate(projectId, { $inc: { likes: -1 }, $pull: { likedBy: username } });
+            }
+        }
+        if (saveproject) {
+            if (!alreadyLiked) {
+                // Increment likes if user has not liked before
+                newLikes++;
+                await SaveProject.findOneAndUpdate(
+                    { '_id': projectId },
+                    {
+                        $inc: { 'project.likes': 1 },
+                        $push: { 'project.likedBy': username }
+                    }
+                );
+            } else {
+                // Decrement likes if user has already liked before
+                newLikes--;
+                await SaveProject.findOneAndUpdate(
+                    { '_id': projectId },
+                    {
+                        $inc: { 'project.likes': -1 },
+                        $pull: { 'project.likedBy': username }
+                    }
+                );
+            }
         }
 
         res.status(200).json({ likes: newLikes });
--- utils/checkUserLikedStatus.js
+++ utils/checkUserLikedStatus.js
@@ -1,17 +1,22 @@
 const Project = require('../models/projects');
+const SaveProject=require('../models/collections');
 
 async function checkUserLikedStatus(projectId, username) {
     try {
         const project = await Project.findById(projectId);
-
-        if (!project) {
+        const saveproject=await SaveProject.findById(projectId);
+        if (!project && !saveproject) {
+            console.log("Fail");
             return false; // Project not found
         }
-
+        if(project){
         const likedBy = project.likedBy;
-
-        // Check if userId exists in likedBy array
         return likedBy.includes(username);
+        }
+        if(saveproject){
+         const likedBy=saveproject.project.likedBy;
+         return likedBy.includes(username);
+        }
     } catch (error) {
         console.error('Error checking user liked status:', error);
         return false;
