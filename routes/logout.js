810e61246d684952981dff7957017b5c4b0c7f01
afaq-ahmed07 <afaqshahbaz9@gmail.com>
2025-02-20 21:31:47 +0500

    changed error and logout files

--- routes/logout.js
+++ routes/logout.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
         // Clear the authToken cookie
-    res.cookie('authToken', '', { httpOnly: true, secure: true, expires: new Date(0) }); // Expire the cookie immediately
+    res.cookie('authToken', '', { httpOnly: true, secure: true, expires: new Date(0) });
     res.redirect('/signin')
 });
 
